cb3cd27463a09c6f06b96af3e882dfaeb80913bc
lissa <bmf3gm@virginia.edu>
2026-06-11 14:28:54 -0400

    updated js with template string

--- index.js
+++ index.js
@@ -5,13 +5,20 @@ const ulEl = document.getElementById("ul-el");
 
 inputButton.addEventListener("click", function() {
     myLeads.push(inputEl.value);
+    inputEl.value = "";
     renderLeads();
 });
 
 function renderLeads() {
     let listItems = "";
     for (i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li>";
+        listItems += ` 
+        <li>
+            <a target='_blank' href='${myLeads[i]}'>
+                ${myLeads[i]}
+            </a>
+        </li>
+        `
     }
     ulEl.innerHTML = listItems;
 }
